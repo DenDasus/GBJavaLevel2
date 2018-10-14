ab6ad5e0f42ce6e6f7b27f4c741101a18086b39b
DenisK <kuzovin@gmail.com>
2018-10-14 21:45:28 +0300

    GameWindow

---
+++ Game/GameWindow.java
@@ -0,0 +1,12 @@
+package Game;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+    public GameWindow() {
+        setTitle("Game");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 400);
+        setVisible(true);
+    }
+}
