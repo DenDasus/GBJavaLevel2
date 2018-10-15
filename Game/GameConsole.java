4eb32ade74ebd3f39e32d9aad45a509a03d3b4a1
DenisK <kuzovin@gmail.com>
2018-10-15 11:57:42 +0300

    console version

--- Game/GameConsole.java
+++ Game/GameConsole.java
@@ -2,50 +2,31 @@ package Game;
 
 import java.util.Random;
 
-class GameConsole {
+class GameConsole implements GameLog {
     public static void main(String[] args) {
+        new GameConsole();
+    }
+    
+    public GameConsole() {
+        Game game = new Game(this);
+        game.addHero(Game.Teams.TEAM1, Hero.HeroTypes.WARRIOR, "Тигрил");
+        game.addHero(Game.Teams.TEAM1, Hero.HeroTypes.ASSASSIN, "Акали");
+        game.addHero(Game.Teams.TEAM1, Hero.HeroTypes.DOCTOR, "Жанна");
+    
+        game.addHero(Game.Teams.TEAM2, Hero.HeroTypes.WARRIOR, "Минотавр");
+        game.addHero(Game.Teams.TEAM2, Hero.HeroTypes.ASSASSIN, "Джинкс");
+        game.addHero(Game.Teams.TEAM2, Hero.HeroTypes.DOCTOR, "Зои");
         
-        Random randomStep = new Random();
-        Random randomHealing = new Random();
-        int round = 3;
-        
-        Hero[] team1 = new Hero[]{new Warrior(250, "Тигрил", 50, 0)
-                , new Assassin(150, "Акали", 70, 0)
-                , new Doctor(120, "Жанна", 0, 60)};
-        
-        
-        Hero[] team2 = new Hero[]{new Warrior(290, "Минотавр", 60, 0)
-                , new Assassin(160, "Джинкс", 90, 0)
-                , new Doctor(110, "Зои", 0, 80)};
-        
-        
-        for (int j = 0; j < round; j++) {
-            for (int i = 0; i < team1.length; i++) {
-                if (randomStep.nextInt(2) == 0) {
-                    if (team1[i] instanceof Doctor) {
-                        team1[i].healing(team1[randomHealing.nextInt(2)]);
-                    } else {
-                        team1[i].hit(team2[i]);
-                    }
-                } else {
-                    if (team2[i] instanceof Doctor) {
-                        team2[i].healing(team2[randomHealing.nextInt(2)]);
-                    } else {
-                        team2[i].hit(team1[i]);
-                    }
-                }
-            }
-        }
-        
-        System.out.println("---------------");
-        
-        for (Hero t1 : team1) {
-            t1.info();
-        }
-        
-        for (Hero t2 : team2) {
-            t2.info();
-        }
+        game.startGame(4);
+    }
+    
+    @Override
+    public void clearLog() {
+    
+    }
+    
+    @Override
+    public void addToLog(String text) {
+        System.out.println(text);
     }
 }
-
