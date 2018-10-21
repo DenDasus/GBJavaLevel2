c06465c75948cab9dd6a84b2b0eac70e3359e176
DenisK <kuzovin@gmail.com>
2018-10-21 19:27:58 +0300

    background and small changes

--- sample/Controller.java
+++ sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class Controller {
 
